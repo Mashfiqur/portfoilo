25be29a580a55b0d56829502b390e529b3e9a043
Mashfiq-Office-Desktop <mashfiqurrr@gmail.com>
2020-12-22 11:13:45 +0600

    first upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Button, Nav,Form, FormControl,Container } from 'react-bootstrap';
+import { BrowserRouter as Router,Switch,Route, Link } from "react-router-dom";
+import Profile from './Pages/Profile/profile';
+import Information from './Pages/Information/information';
+import Education from './Pages/Education/education';
+import Skill from './Pages/Skills/skills';
+import Certification from './Pages/Certification/certification';
+import Experience from './Pages/Experience/experience';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <Router>
+      <Navbar className="nav" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Profile</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav Link to="/information">Information</Nav>
+          
+
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+      </Navbar>
+      
+          <Switch>
+            <Route path="/skill" component={ Skill } />
+            <Route path="/certification">
+              <Certification />
+            </Route>
+            <Route path="/experience">
+              <Experience />
+            </Route>
+            <Route path="/education">
+              <Education />
+            </Route>
+            <Route path="/information">
+              <Information />
+            </Route>
+            <Route path="/">
+              <Profile />
+            </Route>
+          </Switch>
+      </Router>
+      </Container>
     </div>
   );
 }
